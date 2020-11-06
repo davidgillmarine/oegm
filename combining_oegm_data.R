58b7fb73a379884db471fa570518f2e987640556
davidgillmarine <gilldavida@gmail.com>
2020-11-06 15:53:58 -0500

    fuzzy matching titles

--- combining_oegm_data.R
+++ combining_oegm_data.R
@@ -1,5 +1,5 @@
 ##Randomly select citations
-pacman::p_load(rio,rcrossref,bib2df,revtools,janitor,tidyverse)
+pacman::p_load(rio,rcrossref,bib2df,revtools,fuzzyjoin,janitor,tidyverse)
 
 workdir <- "R:/Gill/research/oegm/"
 #workdir <- gsub("git","data",getwd())
@@ -9,6 +9,11 @@ outputdir <- paste0(workdir,"output/tables/")
 plotdir <- paste0(workdir,"output/plots/")
 today.date <- gsub("-","",Sys.Date())
 list.files(inputdir)
+
+last.file <- function(dir.nam,nam){
+  import(paste0(dir.nam,last(sort(grep(nam,list.files(dir.nam), value=T)))))
+}
+
 
 ##Read in file
 # data <- ReadBib("10_OEGM.bib", check = FALSE)
@@ -62,32 +67,85 @@ comb.data <- unique_90.edit %>%
 export(comb.data,paste0(outputdir,today.date,"_oegm_all.rds"))
 export(comb.data,paste0(outputdir,today.date,"_oegm_all.csv"))
 
-
-# --- Update 
+#######################################################################
+# --- Updating 
+#######################################################################
 data <- last.file(outputdir,"_oegm_all.rds")
+table(data$batch)
 
 data.edit <- data %>% 
   mutate(across(c(title,abstract),list(z=~replace_na(.,"")))) %>%  # replace NAs with "" (for combining strings)
   mutate(across(c(title_z,abstract_z),~str_to_lower(.))) %>%   # lower case
   mutate(across(c(title_z,abstract_z),~str_replace_all(.,"\\W", ""))) %>% # remove anything that isn't a word character
-  mutate(rec.id=str_c(str_sub(title_z,1,5),
+  mutate(rec.id=paste(str_sub(title_z,1,16),
                       year,
-                      str_sub(abstract_z,1,5),
+                      str_sub(abstract_z,1,25),
                       sep="_")) %>% 
   mutate(temp.id=paste(str_sub(title_z,1,16),
                        year,
                        sep="_")) 
 
-head(data.edit$rec.id)  
-nrow(data.edit %>% get_dupes(rec.id))
-View(data.edit %>% get_dupes(rec.id))
+head(data.edit$rec.id)
+# Check for duplicates
+excl.test <- filter(data.edit,batch=="unique_10_exclude") %>%  select(rec.id,title, abstract, year,batch) 
+View(excl.test %>% get_dupes(rec.id))
+
+u90.test <- filter(data.edit,batch=="unique_90")%>%  select(rec.id,title, abstract, year,batch) 
+View(u90.test %>% get_dupes(rec.id))
+
+test <- excl.test %>% 
+  inner_join(u90.test,by="rec.id")
+
+excl.dupl <- data.edit %>% 
+  select(rec.id,title, abstract, year,batch) %>% 
+  filter(batch%in%c("unique_90","unique_10_exclude")) %>% 
+  get_dupes(rec.id)
+  
+export(excl.dupl,paste0(outputdir,today.date,"_10exclude_unique90_duplicates.csv"))
+
+
+#  ID full screen data
+data.include <- data.edit %>% 
+  filter(batch=="unique_10_include") %>% 
+  select(title,year,journal,author) %>% 
+  mutate(across(c(title),~str_replace_all(.,"\\W", "")))  # remove anything that isn't a word character
 
 full_include<-import(paste0(inputdir,"20191022_All Data.xlsx"), skip =2, .name_repair = "universal") %>% 
   filter(Full.text.screening.=="Accept") %>% 
   distinct(Article.ID, .keep_all = T) %>% 
   select(Article.ID,
          title=Title,
-         year=Year.of.publication)  %>% 
+         year=Year.of.publication,
+         author=Author.s.) %>% 
+  mutate(across(c(title),~str_replace_all(.,"\\W", "")))  # remove anything that isn't a word character
+  
+  
+full_include %>% 
+  summarise(across(everything(),n_distinct))
+
+
+test <- full_include %>% 
+  stringdist_left_join(data.include, by = c("title" = "title"), max_dist = 9, distance_col = "distance") %>% 
+  mutate(one_correct = title.x == title.y)
+test %>% 
+  summarise(across(everything(),n_distinct))
+found.id <-  test$Article.ID[!is.na(test$title.y)]
+test1 <- full_include %>% 
+  filter(!Article.ID%in%found.id) %>% 
+  stringdist_left_join(data.include, by = c("title" = "title"), max_dist = 20, distance_col = "distance") %>% 
+  mutate(one_correct = title.x == title.y)
+test1 %>% 
+  summarise(across(everything(),n_distinct))
+found.id <-  c(found.id,test1$Article.ID[!is.na(test1$title.y)])
+test2 <- full_include %>% 
+  filter(!Article.ID%in%found.id & title!="NA") %>% 
+  stringdist_left_join(data.include, by = c("title" = "title"), max_dist = 30, distance_col = "distance") %>% 
+  mutate(one_correct = title.x == title.y)
+test2 %>% 
+  summarise(across(everything(),n_distinct))
+found.id <-  c(found.id,test2$Article.ID[!is.na(test2$title.y)])
+
+ 
   mutate(across(c(title),list(z=~replace_na(.,"")))) %>%  # replace NAs with "" (for combining strings)
   mutate(across(c(title_z),~str_to_lower(.))) %>%   # lower case
   mutate(across(c(title_z),~str_replace_all(.,"\\W", ""))) %>% # remove anything that isn't a word character
