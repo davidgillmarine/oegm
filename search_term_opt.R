2e94290741fade51c5976e677bb746e6bace8621
davidgillmarine <gilldavida@gmail.com>
2020-11-10 13:40:31 -0500

    litSearchR

--- search_term_opt.R
+++ search_term_opt.R
@@ -15,8 +15,11 @@ library(dplyr)
 search_directory <- "~/Documents/github/cons_hwb_2020/litsearchr/og_excluded/"
 workdir <- "R:/Gill/research/oegm/"
 search_directory <- paste0(workdir,"tables/raw/litsearchR")
+outputdir <- paste0(workdir,"output/tables/")
+today.date <- gsub("-","",Sys.Date())
 
 naiveimport <- import_results(directory = search_directory, verbose=TRUE)
+#naiveimport <- import_results(directory = paste0(search_directory, "/test"), file="OEGM-Set2_10_included_overlap.ris", verbose=TRUE)
 
 #naiveresults <- remove_duplicates(naiveimport, field = "title", method = "string_osa")
 
@@ -73,7 +76,7 @@ searchterms <- sort(igraph::strength(reducedgraph), decreasing = TRUE)
 
 head(searchterms, 20)
 
-write.csv(searchterms, "search_terms_reduced.csv")
+write.csv(searchterms, str_c(outputdir,today.date,"_search_terms_reduced.csv"))
 # manually group terms in the csv using a spreadsheet application
 
 # read grouped CSV back in
