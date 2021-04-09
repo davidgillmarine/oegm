bb09befca6ded9c8ac7e54f408a0801c72cd6e3b
davidgillmarine <gilldavida@gmail.com>
2021-04-09 11:24:43 -0400

    - add code for bar plots to ID studies by discipline - update efficiency test script

--- effiency_set_splitting.R
+++ effiency_set_splitting.R
@@ -2,17 +2,21 @@ pacman::p_load(rio,hrbrthemes,viridis,revtools,janitor,tidyverse)
 workdir <- "R:/Gill/research/oegm/"
 #workdir <- gsub("git","data",getwd())
 inputdir <- paste0(workdir,"tables/raw/efficency_sets/")
-outputdir <- inputdir
-plotdir <- paste0(workdir,"output/plots/")
+outputdir <- paste0(workdir,"output/tables/")
 today.date <- gsub("-","",Sys.Date())
 
 list.files(inputdir)
-screened.set <- import(paste0(inputdir,"OEGM Set 2c_screened.csv"))
+screened.set <- import(paste0(inputdir,"OEGM Set 3c_screened.csv"))
+names(screened.set) <- gsub("citation_","",names(screened.set))
 
-table(screened.set$citation_screening_status)
-exclude.set <- filter(screened.set,citation_screening_status=="excluded")
-write_bibliography(exclude.set,str_c(outputdir,"OEGM Set 2c_screened_excl.ris")) # export to try to find missing abstracts
 
-include.set <- filter(screened.set,citation_screening_status=="included")
-write_bibliography(exclude.set,str_c(outputdir,"OEGM Set 2c_screened_incl.ris")) # export to try to find missing abstracts
+table(screened.set$screening_status)
+
+exclude.set <- filter(screened.set,screening_status=="excluded")
+exclude.set <- filter(screened.set,screening_status%in%c("excluded","conflict"))
+include.set <- filter(screened.set,screening_status=="included")
+
+write_bibliography(exclude.set,str_c(outputdir,"OEGM Set 4c_screened_excl.ris")) # export to try to find missing abstracts
+
+write_bibliography(include.set,str_c(outputdir,"OEGM Set 4c_screened_incl.ris")) # export to try to find missing abstracts
 
